1abe75c5146e09510212785cfb57ffb469f6e391
Quirmbach <ib7jbn@net.plm.eds.com>
2016-10-31 14:16:55 +0100

    test

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { Router, Params } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+
+  constructor(@Inject(Router) private _router: Router)
+  { }
+
+  public setUrlParams(params: Params) {
+    console.log(params)
+  }
+}
